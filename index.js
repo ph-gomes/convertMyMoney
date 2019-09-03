2487a220bac841cff7e88ea97b0fea5eec4f1b8f
ph-gomes <radago@hotmail.com.br>
2019-09-03 12:49:23 -0300

    Configuração para o zeit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require("path");
 const convert = require("./lib/convert");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
